39d1353b0b29254046da5399d6f3a97dc525489a
loicprodhom <54361074+loicprodhom@users.noreply.github.com>
2020-05-22 16:21:23 +0300

    Updated about screen

--- src/views/AboutScreen.js
+++ src/views/AboutScreen.js
@@ -1,10 +1,30 @@
 import React from "react";
-import { Grid } from "@material-ui/core";
+import { Grid, Typography, Link } from "@material-ui/core";
 
 const AboutScreen = () => {
   return (
-    <Grid>
-      <div>Placeholder about screen</div>
+    <Grid container direction="column" alignContent="center">
+      <Grid item>
+        <Typography variant="h2">Arduino Manager Thesis Project</Typography>
+      </Grid>
+      <Grid item>
+        <Typography variant="body1">
+          This app is developed for my thesis project. Its purpose is to
+          simplify management of specific Arduino prototypes I have connected to
+          the network.
+        </Typography>
+      </Grid>
+      <Grid item>
+        <Typography variant="body1">
+          GitHub repository can be found{" "}
+          <Link
+            target="_blank"
+            href="https://github.com/loicprodhom/arduino-manager-client"
+          >
+            here
+          </Link>
+        </Typography>
+      </Grid>
     </Grid>
   );
 };
