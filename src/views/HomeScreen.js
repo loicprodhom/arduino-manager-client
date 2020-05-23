0cc92c36f2ad569db368501b22304dd781696dac
loicprodhom <54361074+loicprodhom@users.noreply.github.com>
2020-05-23 17:09:31 +0300

    Implemented HEG palette

--- src/views/HomeScreen.js
+++ src/views/HomeScreen.js
@@ -1,7 +1,31 @@
 import React, { useState } from "react";
-import { AppBar, Tabs, Tab, Typography } from "@material-ui/core";
+import {
+  AppBar,
+  Tabs,
+  Tab,
+  Typography,
+  ThemeProvider,
+} from "@material-ui/core";
+import { createMuiTheme } from "@material-ui/core/styles";
 import AboutScreen from "./AboutScreen";
 import DeviceView from "./DeviceView";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: "#616161",
+      main: "#212121",
+      dark: "#212121",
+      contrastText: "#fff",
+    },
+    secondary: {
+      light: "#e53935",
+      main: "#c62828",
+      dark: "#b71c1c",
+      contrastText: "#fff",
+    },
+  },
+});
 
 const HomeScreen = (props) => {
   const [currTab, setCurrTab] = useState("deviceView");
@@ -11,7 +35,7 @@ const HomeScreen = (props) => {
   };
 
   return (
-    <div>
+    <ThemeProvider theme={theme}>
       <AppBar position="static">
         <Typography variant="h6" color="inherit">
           Arduino Manager v.01
@@ -23,7 +47,7 @@ const HomeScreen = (props) => {
       </AppBar>
       {currTab === "deviceView" && <DeviceView client={props.client} />}
       {currTab === "aboutScreen" && <AboutScreen />}
-    </div>
+    </ThemeProvider>
   );
 };
 
