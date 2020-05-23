04e191d1febbfebfbfbd4085f9f56c44dd93b02f
loicprodhom <54361074+loicprodhom@users.noreply.github.com>
2020-05-23 14:02:29 +0300

    Implemented LCD Data Display

--- src/views/LCDBoxComponents/LCDBoxDataDisplay.js
+++ src/views/LCDBoxComponents/LCDBoxDataDisplay.js
@@ -1,8 +1,22 @@
 import React from "react";
+import { Grid, Typography } from "@material-ui/core";
 
 const LCDBoxDataDisplay = (props) => {
   const box = props.box;
-  return <div>LCD Data</div>;
+  return (
+    <Grid container direction="column">
+      <Grid item>
+        <Typography variant="h6">Current display:</Typography>
+      </Grid>
+      <Grid item>
+        {box.data.map((line, index) => (
+          <Typography key={index} variant="body1">
+            Line {index + 1}: "{line}"
+          </Typography>
+        ))}
+      </Grid>
+    </Grid>
+  );
 };
 
 export default LCDBoxDataDisplay;
